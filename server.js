92a0921a91f4a6ec95e1f932cfc1d59f5ad2491d
snoopy910 <snoopy5473cool@gmail.com>
2024-05-31 09:35:11 +0100

    feat: test vercel

--- server.js
+++ server.js
@@ -10,22 +10,22 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-db.sequelize
-  .sync()
-  .then(() => {
-    console.log("synced db");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db " + err.message);
-  });
+// const db = require("./app/models");
+// db.sequelize
+//   .sync()
+//   .then(() => {
+//     console.log("synced db");
+//   })
+//   .catch((err) => {
+//     console.log("Failed to sync db " + err.message);
+//   });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to toy labelling bilby" });
 });
 
-require("./app/routes/document.routes")(app);
-require("./app/routes/label.routes")(app);
+// require("./app/routes/document.routes")(app);
+// require("./app/routes/label.routes")(app);
 
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
